00e487fb4b5ca9da89d47c8dc7c860ad143e5bb1
mark.lonr@tom.com <mark.lonr@tom.com@9f59850f-4995-6775-7c1f-ed900010ca4b>
2011-08-19 07:33:38 +0000

    StdExp:  + 添加容器的查找接口  + 为CStringT的一些接口添加返回值  + 添加CStringT::Trim()

--- trunk/WinExp/StdExp/Container/ContainerObject.h
+++ trunk/WinExp/StdExp/Container/ContainerObject.h
@@ -33,13 +33,14 @@
 // Author:	木头云
 // Home:	dark-c.at
 // E-Mail:	mark.lonr@tom.com
-// Date:	2011-07-28
-// Version:	1.0.0011.1458
+// Date:	2011-08-19
+// Version:	1.0.0012.1050
 //
 // History:
 //	- 1.0.0009.1617(2011-06-02)	+ 添加默认的IContainerObjectT::Del()接口,支持直接定位元素删除
 //	- 1.0.0010.0142(2011-06-12)	# 修正默认的IContainerObjectT::Del()接口与容器子类的Del()的重载冲突
 //	- 1.0.0011.1458(2011-07-28)	= 将容器的迭代器接口调整为const类型
+//	- 1.0.0012.1050(2011-08-19)	+ 添加容器的查找接口
 //////////////////////////////////////////////////////////////////
 
 #ifndef __ContainerObject_h__
@@ -100,8 +101,26 @@ public:
 	type_t& LastItem() const
 	{ return This()->container_t::LastItem(); }
 
+	iterator_t& Find(iterator_t& Head, iterator_t& Tail, const type_t& Item)
+	{ return finder_t::Find(Head, Tail, Item); }
+	iterator_t& Find(const type_t& Item)
+	{ return finder_t::Find(*(This()), Item); }
+	iterator_t& Find(iterator_t& Head, iterator_t& Tail, const container_t& cnt2)
+	{ return finder_t::Find(Head, Tail, cnt2); }
+	iterator_t& Find(const container_t& cnt2)
+	{ return finder_t::Find(*(This()), cnt2); }
+
+	iterator_t& RevFind(iterator_t& Head, iterator_t& Tail, const type_t& Item)
+	{ return finder_t::RevFind(Head, Tail, Item); }
+	iterator_t& RevFind(const type_t& Item)
+	{ return finder_t::RevFind(*(This()), Item); }
+	iterator_t& RevFind(iterator_t& Head, iterator_t& Tail, const container_t& cnt2)
+	{ return finder_t::RevFind(Head, Tail, cnt2); }
+	iterator_t& RevFind(const container_t& cnt2)
+	{ return finder_t::RevFind(*(This()), cnt2); }
+
 	bool Replace(type_t& Type)
-	{ return This()->container_t::Del(finder_t::Find(*(This()), Type)); }
+	{ return This()->container_t::Del(Find(Type)); }
 };
 
 //////////////////////////////////////////////////////////////////

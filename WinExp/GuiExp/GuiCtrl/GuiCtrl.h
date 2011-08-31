c585a130973b2b602f86ff919227b2ffd4d090c5
mark.lonr <mark.lonr@tom.com>
2011-08-31 05:22:50 +0000

    GuiExp:  ^ 当IGuiCtrl::SetScroll()传入的参数与当前m_Scroll相等,将不进行m_Scroll的刷新操作
    ImgExp:
     = 调整CRectT::IsEmpty()接口,当区域大小为负数时也判定为true

--- WinExp/GuiExp/GuiCtrl/GuiCtrl.h
+++ WinExp/GuiExp/GuiCtrl/GuiCtrl.h
@@ -33,8 +33,8 @@
 // Author:	木头云
 // Home:	dark-c.at
 // E-Mail:	mark.lonr@tom.com
-// Date:	2011-08-25
-// Version:	1.0.0015.1643
+// Date:	2011-08-31
+// Version:	1.0.0016.1318
 //
 // History:
 //	- 1.0.0001.2236(2011-05-23)	+ IGuiCtrl添加效果对象相关接口
@@ -57,6 +57,7 @@
 //	- 1.0.0013.1730(2011-08-11)	= IGuiCtrl在SetFocus时先发送焦点失去消息,再发送焦点获得消息
 //	- 1.0.0014.1816(2011-08-24)	+ 当IGuiCtrl添加一个Scroll对象时,自动对它设置main属性
 //	- 1.0.0015.1643(2011-08-25)	^ 大幅简化IGuiCtrl::GetState()接口的调用方式及效率
+//	- 1.0.0016.1318(2011-08-31)	^ 当IGuiCtrl::SetScroll()传入的参数与当前m_Scroll相等,将不进行m_Scroll的刷新操作
 //////////////////////////////////////////////////////////////////
 
 #ifndef __GuiCtrl_h__
@@ -199,7 +200,9 @@ public:
 	}
 	void SetScroll(IGuiCtrl* pScroll)
 	{
-		if (m_Scroll = (IGuiCtrl*)pScroll)
+		IGuiCtrl* old_scr = m_Scroll;
+		m_Scroll = pScroll;
+		if (m_Scroll && old_scr != m_Scroll)
 		{
 			m_Scroll->SetState(_T("main"), this);
 			SetScrollSize(m_szScroll, true);

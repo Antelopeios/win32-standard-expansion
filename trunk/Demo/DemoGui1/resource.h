ba25a2e3b4026c45e904e91e2e787a3b6f4c1898
mark.lonr@tom.com <mark.lonr@tom.com@9f59850f-4995-6775-7c1f-ed900010ca4b>
2011-11-08 07:34:39 +0000

    GuiExp:  # 添加APSTUDIO_INVOKED宏定义,防止编译时报错

--- trunk/Demo/DemoGui1/resource.h
+++ trunk/Demo/DemoGui1/resource.h
@@ -9,6 +9,8 @@
 #define IDR_MAINFRAME                   128
 #define IDC_STATIC                      -1
 
+#define APSTUDIO_INVOKED
+
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED

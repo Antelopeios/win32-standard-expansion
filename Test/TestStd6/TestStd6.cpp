aff33ab9161403e3f9cecae446a930076f6636a1
mark.lonr <mark.lonr@tom.com>
2011-12-13 15:20:04 +0000

    StdExp:  @ 基本完成崩溃捕获模块

---
+++ Test/TestStd6/TestStd6.cpp
@@ -0,0 +1,29 @@
+// TestStd6.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+//////////////////////////////////////////////////////////////////
+
+class CCrash : public ICrashHandler
+{
+public:
+	void OnCrash()
+	{
+		ExMPrintf(_T("好吧,我挂了...=.=\n崩溃转储在这里: %s\n崩溃日志在这里: %s"), 
+			(LPCTSTR)GetCrashFile(), (LPCTSTR)GetCrashLog());
+	}
+} g_Crash;
+
+//////////////////////////////////////////////////////////////////
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int c = 10;
+	for(int i = 0; i <= c; ++i)
+	{
+		ExCrash(_T("c = %d, i = %d\n"), c, i);
+		ExCPrintf(_T("%d / %d = %d\n"), c, (c - i), c / (c - i));
+	}
+	return 0;
+}
